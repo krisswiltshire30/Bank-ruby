ef34ac7aa31ee7657f4a611dcef18f88ecb13597
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-09-23 17:02:09 +0100

    Refactored

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -4,10 +4,16 @@ class BankStatement
   attr_accessor :statement
 
   def initialize
-    @statement = 'date || credit || debit || balance'
+    @statement = []
   end
 
-  def print_statement()
-    @statement
+  def print_statement
+    @statement.unshift(%w[date credit debit balance]) if @statement == []
+    @statement.each do |line|
+      puts line.join(' || ')
+    end
   end
 end
+
+# statement = BankStatement.new
+# statement.print_statement
--- spec/features/view_bank_statement_spec.rb
+++ spec/features/view_bank_statement_spec.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
-require './lib/bank_statement.rb'
-# As a user
-# So that I see my bank history
-# I would like to view my bank statement
 
-describe 'View bank statement' do
-  it 'User should be able to view bank statement' do
-    statement = BankStatement.new
-    expect(statement.print_statement).to include('date || credit || debit || balance')
+require './lib/bank_statement.rb'
+
+RSpec.describe BankStatement do
+  subject { BankStatement.new }
+  # As a user
+  # So that I see my bank history
+  # I would like to view my bank statement
+  describe 'Viewing statement' do
+    it 'User should be able to view their bank statement' do
+      expect{ subject.print_statement }.to output("date || credit || debit || balance\n").to_stdout
+    end
   end
 end
