f223160d5a85c79f02b1b1ff8b5ac03750472128
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-09-24 10:51:58 +0100

    Extrated deposit and withdraw classes to transaction class
    Tests updated

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,23 +1,21 @@
 # frozen_string_literal: true
 
 require_relative 'bank_account'
-require_relative 'deposit'
-require_relative 'withdraw'
+require_relative 'transaction'
 
 # Bank controller
 class Bank
   def initialize
     @bank_account = BankAccount.new
-    @deposit = Deposit.new
-    @withdraw = Withdraw.new
+    @transaction = Transaction.new
   end
 
   def deposit(amount)
-    @deposit.make_a_deposit(@bank_account, amount)
+    @transaction.make_a_deposit(@bank_account, amount)
   end
 
   def withdraw(amount)
-    @withdraw.make_a_withdrawal(@bank_account, amount)
+    @transaction.make_a_withdrawal(@bank_account, amount)
   end
 
   def bank_statement
---
+++ spec/transaction_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require './lib/bank.rb'
+require './lib/bank_account.rb'
+require './lib/transaction.rb'
+
+RSpec.describe Transaction do
+  subject { Transaction.new }
+  let(:bank) { Bank.new }
+  let(:bank_account) { BankAccount.new }
+
+  describe 'Withdraw' do
+    it 'Adds the date of withdrawal to bank statement' do
+      Timecop.freeze(Time.local(2019, 9, 13))
+      subject.make_a_deposit(bank_account, 2000)
+      subject.make_a_withdrawal(bank_account, 1000)
+      expect(bank_account.statement[0][0]).to eq('13/09/19')
+    end
+
+    it 'Adds the withdraw amount to bank statement' do
+      subject.make_a_deposit(bank_account, 2000)
+      subject.make_a_withdrawal(bank_account, 1000)
+      expect(bank_account.statement[0][2]).to eq('1000.00')
+    end
+
+    it 'Calculates the total balance' do
+      subject.make_a_deposit(bank_account, 1000)
+      subject.make_a_withdrawal(bank_account, 995)
+      expect(bank_account.statement[0][3]).to eq('5.00')
+    end
+  end
+
+  describe 'Deposit' do
+    it 'Adds the date of deposit to bank statement' do
+      Timecop.freeze(Time.local(2019, 9, 13))
+      subject.make_a_deposit(bank_account, 1000)
+      expect(bank_account.statement[0][0]).to eq('13/09/19')
+    end
+
+    it 'Adds the deposit amount bank statement' do
+      subject.make_a_deposit(bank_account, 1000)
+      expect(bank_account.statement[0][1]).to eq('1000.00')
+    end
+
+    it 'Calculates the total balance' do
+      subject.make_a_deposit(bank_account, 1000)
+      subject.make_a_deposit(bank_account, 1000)
+      expect(bank_account.statement[0][3]).to eq('2000.00')
+    end
+  end
+end
